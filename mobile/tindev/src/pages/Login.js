c10a11d663f712e2bf2d8985a4fdbfed13c8151c
soucer10 <hugobicudo0@gmail.com>
2019-08-31 02:04:51 -0300

    Criação da interface da tela de Login

---
+++ mobile/tindev/src/pages/Login.js
@@ -0,0 +1,58 @@
+import React from  'react'
+import {Text, View, StyleSheet, Image, TextInput,Button,TouchableOpacity} from 'react-native'
+
+import Logo from '../assets/logo.png'
+
+
+export default function Login(){
+    return(<View style={styles.container}>
+        <Image source={Logo}/>
+        <TextInput placeholder="Digite seu usuário do Github"
+            placeholderTextColor='#999'
+            style={styles.input}
+            autoCapitalize='none'
+            autoCorrect={false}
+        />
+        <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Enviar</Text>
+        </TouchableOpacity>
+    </View>)
+       
+}
+
+
+
+const styles=StyleSheet.create({
+
+    container:{
+        flex:1,
+        backgroundColor:'#f5f5f5',
+        justifyContent:'center',
+        alignItems:'center',
+        padding:30,
+    },
+    input:{
+        height:46,
+        alignSelf:'stretch',
+        backgroundColor:'#FFF',
+        borderWidth:1,
+        borderColor:'#ddd',
+        borderRadius:4,
+        marginTop:20,
+        paddingHorizontal:15,
+    },
+    button:{
+        height:46,
+        alignSelf:'stretch',
+        backgroundColor:'#DF4723',
+        borderRadius:4,
+        marginTop:10,
+        justifyContent:'center',
+        alignItems:'center',
+    },
+    buttonText:{
+        color:'#FFF',
+        fontSize:16,
+        fontWeight: 'bold',
+    },
+})
