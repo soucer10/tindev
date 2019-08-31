d8a2e0755a275a07709c1f5edde33dd94cc0f845
soucer10 <hugobicudo0@gmail.com>
2019-08-31 12:37:16 -0300

    Adicionando variáveis de ambiente no backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,11 +2,10 @@ const express=require('express');
 const mongoose=require('mongoose');
 const rotas =require('./rotas');
 const cors=require('cors')
-
+require('dotenv/config')
 
 const server=express();
-mongoose.connect('mongodb+srv://soucer10:soucer10@cluster0-fwaym.mongodb.net/omnistack8?retryWrites=true&w=majority',{useNewUrlParser:true});
-
+mongoose.connect(`mongodb+srv://${process.env.MONGO_LOGIN}:${process.env.MONGO_PASS}@cluster0-fwaym.mongodb.net/omnistack8?retryWrites=true&w=majority`,{useNewUrlParser:true}); 
 server.use(cors());
 server.use(express.json());
 server.use(rotas);
